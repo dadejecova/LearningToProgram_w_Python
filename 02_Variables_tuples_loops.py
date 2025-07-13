461d5c1bb00ba9efd7e8673fc3112396ca44ba7e
Daniel <danielcoellovargas@gmail.com>
2025-07-12 20:10:34 -0500

    Add Hello World and variables/tuples/loops examples
    Added 01_HelloWorld.py with a simple hello world print statement. Added 02_Variables_tuples_loops.py demonstrating variable assignment, tuple usage, type checking, and iterating over a tuple.

---
+++ 02_Variables_tuples_loops.py
@@ -0,0 +1,11 @@
+#Variable is a word representation of a value in a program.
+
+#Variables can be used to store data that can be used later in the program.
+
+programming_languages = 'Python', 'Java', 'C++' #This is a tuple, which is an immutable sequence of values.
+print(type(programming_languages)) #This will print the type of the variable, which is a tuple.
+print(programming_languages) #This will print the tuple of programming languages.
+
+
+for language in programming_languages:
+    print(language)  # This will print each programming language in the tuple.
