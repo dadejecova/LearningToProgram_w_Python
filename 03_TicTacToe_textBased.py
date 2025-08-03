16ee6e9fd62b893e0d95577090d5ec6f8d185665
Daniel <danielcoellovargas@gmail.com>
2025-08-03 10:34:26 -0500

    Update 03_TicTacToe_textBased.py

--- 03_TicTacToe_textBased.py
+++ 03_TicTacToe_textBased.py
@@ -1,17 +1,22 @@
+
 # Using build-in functions.
 tictactoe = [[0,0,0],
              [0,0,0],
              [0,0,0]]
 
 
-def game_board():
+def game_board(player=0, row=0, column=0, just_print=False):
     print (" 0 1 2")
+    if not just_print:
+        tictactoe[row][column] = player
     for count, row in enumerate(tictactoe):
         print(count, row)
 
 
-game_board()
-
+game_board(just_print=True)
+game_board(player=1, row=2, column=1)
+'''
 tictactoe[0][1] = 1
 
 game_board()
+'''
